3b3bd3ef12d61914982931bf9fafdace7c179535
VicLovely <vicky.lubi@gmail.com>
2024-06-02 10:28:00 +0900

    Add files via upload
    CC lab for variables

---
+++ CClab.py
@@ -0,0 +1,34 @@
+name = "Michael Jackson"
+print (name)
+print(name[-1])
+print (name[-15])
+print (len(name))
+print (name[0:4])
+print (name[::2])
+print (name[0:5:2])
+statement = name + " is the best"
+print (statement)
+print (3* name )
+
+#escape sequences 
+print ( "MJ \n is the best") # \n is new line 
+print ("MJ \t is the best") # \t is tab space 
+print ("MJ is \\ the best") # \\ is just \
+print(r" Michael Jackson \ is the best" ) # raw string 
+
+
+# String operations 
+print ("UPPER")
+a = "normal string"
+print ("before upper: ", a)
+b = a.upper()
+print ("After upper: ", b)
+
+print ("LOWER")
+c = "BIG SIZE"
+print (c)
+d = c.lower()
+print (d)
+
+
+
