2aec946ee6123e730ff7e6942f1707f795860f9b
VicLovely <vicky.lubi@gmail.com>
2024-06-14 08:26:18 +0900

    Add files via upload
    bigger lab on sets

---
+++ CClab-sets2.py
@@ -0,0 +1,31 @@
+set1 = {"pop", "rock", "soul", "hard rock", "rock", "R&B", "rock", "disco"}
+print (set1)
+album_list = [ "Michael Jackson", "Thriller", 1982, "00:42:19", "Pop, Rock, R&B", 46.0, 65, "30-Nov-82", None, 10.0]
+album_set = set (album_list)
+print (album_list)
+print (album_set)
+
+numbers_set = set ([1,11,1,2,3,5,5,7,8])
+print (numbers_set)
+
+A = set(["Thriller", "Back in Black", "AC/DC"])
+print (A)
+A.add ("NSYNC")
+print (A)
+A.remove ("NSYNC")
+print (A)
+print ("AC/DC" in A)
+album_set1 = set(["Thriller", 'AC/DC', 'Back in Black'])
+album_set2 = set([ "AC/DC", "Back in Black", "The Dark Side of the Moon"])
+print (album_set1,album_set2)
+intersection = album_set1 & album_set2
+print (intersection)
+print (album_set1.difference(album_set2))
+print (album_set2.difference(album_set1))
+print (album_set1.intersection(album_set2))
+print (album_set2.intersection(album_set1))
+print (album_set1.union(album_set2))
+print (set(album_set1).issuperset(album_set2))
+print (set(album_set2).issubset(album_set1))
+print (set({"Back in Black", "AC/DC"}).issubset(album_set1) )
+print (album_set1.issuperset({"Back in Black", "AC/DC"}) )
