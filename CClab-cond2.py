2aadbdbcd3e9bebef413b0982233dd87b13bc1d2
VicLovely <vicky.lubi@gmail.com>
2024-06-18 11:22:00 +0900

    Add files via upload
    if else lab

---
+++ CClab-cond2.py
@@ -0,0 +1,18 @@
+#Leap year
+annie = 1996
+jane = 1999
+if annie%4==0:
+    print ("ANNIE WAS BORN ON THE LEAR YEAR")
+elif jane%4==0:
+    print ("JANE WAS BORN ON THE LEAR YEAR")
+else: 
+    print("None of them were born in a leap year")
+
+  #Canteen 
+age = 10  
+if age <= 9:
+    print ("Milk poridge")
+elif age >= 10 and age <=14:
+    print ("Sandwitch")
+elif age >= 15 and age <=17:
+    print ("Burger")
